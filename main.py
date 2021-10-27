1365dc34e3a98a436060833f4eb39d9a3eb180cc
Robin Groot <robingroot1999@hotmail.com>
2021-10-27 17:30:04 +0200

    updates

--- main.py
+++ main.py
@@ -1,47 +1,8 @@
 import string
 import random
 import csv
-import secrets
-from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
-
 from cryptography.fernet import Fernet
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import hashes
-from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
-
-# Initialise
-backend = default_backend()
-iterations = 100_000
-pw_correct = False
-
-
-# Defining functions
-def _derive_key(password: bytes, salt: bytes, iterations: int = iterations) -> bytes:
-    """Derive a secret key from a given password and salt"""
-    kdf = PBKDF2HMAC(
-        algorithm=hashes.SHA256(), length=32, salt=salt,
-        iterations=iterations, backend=backend)
-    return b64e(kdf.derive(password))
-
-
-def password_encrypt(message: bytes, password: str, iterations: int = iterations) -> bytes:
-    salt = secrets.token_bytes(16)
-    key = _derive_key(password.encode(), salt, iterations)
-    return b64e(
-        b'%b%b%b' % (
-            salt,
-            iterations.to_bytes(4, 'big'),
-            b64d(Fernet(key).encrypt(message)),
-        )
-    )
-
-
-def password_decrypt(token: bytes, password: str) -> bytes:
-    decoded = b64d(token)
-    salt, iter, token = decoded[:16], decoded[16:20], b64e(decoded[20:])
-    iterations = int.from_bytes(iter, 'big')
-    key = _derive_key(password.encode(), salt, iterations)
-    return Fernet(key).decrypt(token)
+from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
 
 
 def generate_random_password():
@@ -92,17 +53,19 @@ def generate_random_password():
     website = input("On what website is this?")
     username = input("What is the associated username or email for this login?")
     message_joined = website + '' + username + '' + password_joined
-    # Encrypts the password with the master password
-    crypt_message = password_encrypt(message_joined.encode(), master_pw)
+
+    # encrypting
+    message_joined = message_joined.encode()
+    encrypted_message = fernet_master_pw.encrypt(message_joined)
 
     # Put info into database csv file
     with open('passwords.csv', 'a') as file:
         writer = csv.writer(file)
-        writer.writerow(crypt_message)
+        writer.writerow(encrypted_message)
 
 
-master_pw = input(f"Pick your master password: ")
-print(master_pw)
+master_pw = input("Pick your master password: ")
+fernet_master_pw = Fernet(master_pw)
 
 while master_pw == master_pw:
     # Generate password based on parameters given.
@@ -112,15 +75,12 @@ while master_pw == master_pw:
         generate_random_password()
     elif what_to_do == "last3":
         # TODO get the last 3 passwords from database and decrypt with master password THIS DOES NOT WORK YET
-        # Open the file and read three lines efficiently.
-        with open('passwords.csv', 'r') as f:  # open in binary for seek operations
-            # Lines created above were 10 bytes long max, go back more than 3x that
-            f.seek(-50, 2)  # 2 means end-of-file
-            lines = f.readlines()  # read only the last few lines of the file
+        with open('passwords.csv') as f:
+            f = bytes(f)
+            last3 = list(f)[-3:]
+            decrypted = fernet_master_pw.decrypt(f)
+            print(decrypted)
 
-        token = bytes("token")
-        for line in lines[-3:]:  # slice only the last three lines and display.
-            print(password_decrypt(token, master_pw).decode())
     elif what_to_do == "close":
         exit()
     else:
