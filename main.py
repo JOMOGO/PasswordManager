a2b95ffeb3da4d2710ba3d6cab7cb2d1f12bbd9c
Robin Groot <robingroot1999@hotmail.com>
2021-10-29 14:39:42 +0200

    last code updates before correct documentation.

--- main.py
+++ main.py
@@ -1,8 +1,46 @@
 import string
 import random
 import csv
-from cryptography.fernet import Fernet
-from base64 import urlsafe_b64encode as b64e, urlsafe_b64decode as b64d
+import cryptocode
+import os
+
+
+def get_last_n_lines(file_name, n):
+    # Create an empty list to keep the track of last n lines
+    list_of_lines = []
+    # Open file for reading in binary mode
+    with open(file_name, 'rb') as read_obj:
+        # Move the cursor to the end of the file
+        read_obj.seek(0, os.SEEK_END)
+        # Create a buffer to keep the last read line
+        buffer = bytearray()
+        # Get the current position of pointer i.e eof
+        pointer_location = read_obj.tell()
+        # Loop till pointer reaches the top of the file
+        while pointer_location >= 0:
+            # Move the file pointer to the location pointed by pointer_location
+            read_obj.seek(pointer_location)
+            # Shift pointer location by -1
+            pointer_location = pointer_location - 1
+            # read that byte / character
+            new_byte = read_obj.read(1)
+            # If the read byte is new line character then it means one line is read
+            if new_byte == b'\n':
+                # Save the line in list of lines
+                list_of_lines.append(buffer.decode()[::-1])
+                # If the size of list reaches n, then return the reversed list
+                if len(list_of_lines) == n:
+                    return list(reversed(list_of_lines))
+                # Reinitialize the byte array to save next line
+                buffer = bytearray()
+            else:
+                # If last read character is not eol then add it in buffer
+                buffer.extend(new_byte)
+        # As file is read completely, if there is still data in buffer, then its first line.
+        if len(buffer) > 0:
+            list_of_lines.append(buffer.decode()[::-1])
+    # return the reversed list
+    return list(reversed(list_of_lines))
 
 
 def generate_random_password():
@@ -52,11 +90,12 @@ def generate_random_password():
     # Asks for your username and the website
     website = input("On what website is this?")
     username = input("What is the associated username or email for this login?")
-    message_joined = website + '' + username + '' + password_joined
+    message_joined = website + ' | ' + username + ' | ' + password_joined
 
     # encrypting
-    message_joined = message_joined.encode()
-    encrypted_message = fernet_master_pw.encrypt(message_joined)
+    # message_joined = message_joined.encode()
+    type(message_joined)
+    encrypted_message = cryptocode.encrypt(message_joined, master_pw)
 
     # Put info into database csv file
     with open('passwords.csv', 'a') as file:
@@ -65,21 +104,20 @@ def generate_random_password():
 
 
 master_pw = input("Pick your master password: ")
-fernet_master_pw = Fernet(master_pw)
-
 while master_pw == master_pw:
-    # Generate password based on parameters given.
-    what_to_do = input(f"What do you want to do?\nOptions: New, Last3 or close: ")
+    what_to_do = input(f"What do you want to do?\nOptions: New, Last or close: ")
     if what_to_do == "new":
         # Invoking the password generation function.
         generate_random_password()
-    elif what_to_do == "last3":
-        # TODO get the last 3 passwords from database and decrypt with master password THIS DOES NOT WORK YET
-        with open('passwords.csv') as f:
-            f = bytes(f)
-            last3 = list(f)[-3:]
-            decrypted = fernet_master_pw.decrypt(f)
-            print(decrypted)
+    elif what_to_do == "last":
+        last_number = int(input("How many passwords do you want to see from newest to oldest."))
+        # Get last three lines from file 'passwords.csv'
+        last_lines = get_last_n_lines("passwords.csv", last_number+1)
+        print('Last 3 lines of File:\nWebsite | Username | Password')
+        # Iterate over the list of last 3 lines and print one by one
+        for line in last_lines:
+            line = cryptocode.decrypt(line, master_pw)
+            print(line)
 
     elif what_to_do == "close":
         exit()
